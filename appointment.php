96a265dd2e55e315a365b690f17777f1786d8c62
dohuison <thsehgml37@korea.ac.kr>
2021-06-08 04:32:48 +0900

    simple appointment

--- appointment.php
+++ appointment.php
@@ -64,16 +64,37 @@ body{
         <fieldset>
             <legend>미용실 예약</legend>
             <center>
-            <table width="430" height="480">
+            <table width="500" height="700">
                 <tr> 
-                    <td><b>예약하실 회원 성함 </b></td>
+                    <td><b>성함 </b></td>
                     <td><input type="text" name="name"></td>
                 </tr>
                 <tr>
-                    <td><b>예약하실 회원 전화번호 </b></td>
+                    <td><b>전화번호 </b></td>
                     <td><input type="text" name="telephone_number"></td>
                 </tr>
-
+                <tr>
+					<td><b>예약날짜</b></td>
+					<td><input type="date" name="date"></td>
+				</tr>
+                <tr>
+                    <td><b>예약하실 디자이너 </b></td>
+                    <td><label><input type="checkbox" name="did" value="1"> 1 김현우</label></td>
+                    <td><label><input type="checkbox" name="did" value="2"> 2 조성국</label></td>
+                    <td><label><input type="checkbox" name="did" value="3"> 3 박성우</label></td>
+                    <td><label><input type="checkbox" name="did" value="4"> 4 이지은</label></td>
+                    <td><label><input type="checkbox" name="did" value="5"> 5 고범연</label></td>
+                    <td><label><input type="checkbox" name="did" value="6"> 6 심종화</label></td>
+                    <td><label><input type="checkbox" name="did" value="7"> 7 김은빈</label></td>
+                </tr>
+                <tr>
+                    <td><b>예약하실 디자이너 </b></td>
+                    <td><label><input type="checkbox" name="style_menu" value="남성 컷"> 남성 컷</label></td>
+                    <td><label><input type="checkbox" name="style_menu" value="여성 컷"> 여성 컷</label></td>
+                    <td><label><input type="checkbox" name="style_menu" value="열펌"> 열펌</label></td>
+                    <td><label><input type="checkbox" name="style_menu" value="염색"> 염색</label></td>
+                    <td><label><input type="checkbox" name="style_menu" value="일반펌"> 일반펌</label></td>
+                </tr>
                 <!-- submit button -->
                 <tr><td colspan="2"><center><img src="img/confirm.png" width="200" height="50" onclick="document.form.submit();"></center></td></tr>
             </table>
